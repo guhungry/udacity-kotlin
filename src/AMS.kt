5dcf7acdce1dd854c2c06c7ed8062354ffb6b761
Woraphot Chokratanasombat <wc@asiola.com>
2018-05-28 12:09:19 +0700

    [AMS] Change: Add canAddFish()

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,8 +1,17 @@
+import com.sun.org.apache.xpath.internal.operations.Bool
 import java.util.*
 
 fun main(args: Array<String>) {
     // dayOfWeek()
     feedTheFish()
+    println(canAddFish(10.0, listOf(3,3,3)))
+    println(canAddFish(8.0, listOf(2,2,2), hasDecorations = false))
+    println(canAddFish(9.0, listOf(1,1,3), 3))
+    println(canAddFish(10.0, listOf(), 7, true))
+}
+
+fun canAddFish(tankSize: Double, currentFish: List<Int>, fishSize: Int = 2, hasDecorations: Boolean = true): Boolean {
+    return tankSize.times(if (hasDecorations) .8 else 1.0) >= currentFish.sum() + fishSize
 }
 
 fun shouldChangeWater(day: String, temperature: Int = 22, dirtiness: Int = 20): Boolean {
