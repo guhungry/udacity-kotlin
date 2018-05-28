629065ff6d53c66a7743d8d623b2b5f2bbf058d9
Woraphot Chokratanasombat <wc@asiola.com>
2018-05-28 21:16:04 +0700

    [AMS@Aquarium] Change: Add TowerTank

--- src/Aquarium/Aquarium.kt
+++ src/Aquarium/Aquarium.kt
@@ -1,15 +1,23 @@
 package Aquarium
 
-class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
-    var volume: Int
+import kotlin.math.PI
+
+open class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+    open var volume: Int
         get() = width * height * length / 1000
-        set(value) { height = (value * 1000) / (width + height) }
-    var water: Double = volume * .9
+        set(value) { height = (value * 1000) / (width * height) }
+    open var water: Double = volume * .9
 
     constructor(numberOfFish: Int): this() {
         val water = numberOfFish * 2000
         val tank = water + water * 0.1
         height = (tank / (length * width)).toInt()
     }
+}
 
+class TowerTank() : Aquarium() {
+    override var volume: Int
+        get() = (width * height * length / 1000 * PI).toInt()
+        set(value) { height = (value * 1000) / (width * height) }
+    override var water: Double = volume * .8
 }
