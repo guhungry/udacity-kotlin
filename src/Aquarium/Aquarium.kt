7899f1e304c05f459dd5f1050de8c8d5ede9886d
Woraphot Chokratanasombat <wc@asiola.com>
2018-05-28 20:41:57 +0700

    [AMS@Aquarium] Change: Add water, default constructor

--- src/Aquarium/Aquarium.kt
+++ src/Aquarium/Aquarium.kt
@@ -1,11 +1,9 @@
 package Aquarium
 
-class Aquarium {
-    var width: Int = 20
-    var height: Int = 40
-    var length: Int = 100
+class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
     var volume: Int
         get() = width * height * length / 1000
         set(value) { height = (value * 1000) / (width + height) }
-
+    val water: Double
+        get() = volume * .9
 }
--- src/Aquarium/Main.kt
+++ src/Aquarium/Main.kt
@@ -5,13 +5,18 @@ fun main(args: Array<String>) {
 }
 
 private fun buildAquarium() {
-    val myAquarium = Aquarium()
+    val myAquarium = Aquarium(height = 30)
     println(
             "Length: ${myAquarium.length}, " +
             "Width: ${myAquarium.width}, " +
             "Height: ${myAquarium.height}"
     )
+    println("Volume: ${myAquarium.volume}")
+    println("Water: ${myAquarium.water}")
     myAquarium.height = 80
     println("Height: ${myAquarium.height}")
     println("Volume: ${myAquarium.volume}")
+    println("Water: ${myAquarium.water}")
+    val smallAquarium = Aquarium(length = 20, width =  15, height =  30)
+    println("Small Aquarium: Volume = ${smallAquarium.volume}")
 }
