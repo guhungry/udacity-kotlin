0de74abc119521ab0ead1181c099c1a884a3035e
Woraphot Chokratanasombat <wc@asiola.com>
2018-05-28 20:28:01 +0700

    [Quiz] Change: Add Spice

---
+++ src/Spice/Main.kt
@@ -0,0 +1,6 @@
+package Spice
+
+fun main(args: Array<String>) {
+    val spice = SimpleSpice()
+    println("Name: ${spice.name}, Heat: ${spice.heat}")
+}
---
+++ src/Spice/SimpleSpice.kt
@@ -0,0 +1,8 @@
+package Spice
+
+class SimpleSpice {
+    var name: String = "curry"
+    var spiciness: String = "mild"
+    val heat: Int
+        get() = 5
+}
