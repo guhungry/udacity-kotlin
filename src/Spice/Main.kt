0a9dfd6d55cb9e293f883ac9459c2e9f77b1155d
Woraphot Chokratanasombat <wc@asiola.com>
2018-05-28 20:56:57 +0700

    [Quiz@Spice] Change: Add more heat

--- src/Spice/Main.kt
+++ src/Spice/Main.kt
@@ -1,6 +1,19 @@
 package Spice
 
 fun main(args: Array<String>) {
-    val spice = SimpleSpice()
+    val spice = makeSalt()
     println("Name: ${spice.name}, Heat: ${spice.heat}")
+    val spices1 = listOf(
+            SimpleSpice("curry", "mild"),
+            SimpleSpice("pepper", "medium"),
+            SimpleSpice("cayenne", "spicy"),
+            SimpleSpice("ginger", "mild"),
+            SimpleSpice("red curry", "medium"),
+            SimpleSpice("green curry", "mild"),
+            SimpleSpice("hot pepper", "extremely spicy")
+    )
+    val spicelist = spices1.filter {it.heat < 5}
+    println(spicelist)
 }
+
+fun makeSalt() = SimpleSpice("salt")
--- src/Spice/SimpleSpice.kt
+++ src/Spice/SimpleSpice.kt
@@ -1,8 +1,16 @@
 package Spice
 
-class SimpleSpice {
-    var name: String = "curry"
-    var spiciness: String = "mild"
+class SimpleSpice(var name: String, var spiciness: String = "mild") {
     val heat: Int
-        get() = 5
+        get() = when(spiciness) {
+            "mild" -> 1
+            "medium" -> 3
+            "spicy" -> 5
+            "very spicy" -> 7
+            "extremely spicy" -> 10
+            else -> 0
+        }
+    init {
+        println("Name: $name, spiciness: $spiciness, heat: $heat")
+    }
 }
