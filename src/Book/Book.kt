3d46e7c54ea65d07ae357ce8de9ba9afc34487db
Woraphot Chokratanasombat <wc@asiola.com>
2018-05-29 08:41:50 +0700

    [Quiz] Change: Add Book infoTitleAuthor() and infoTitleAuthorYear()

--- src/Book/Book.kt
+++ src/Book/Book.kt
@@ -1,13 +1,21 @@
 package Book
 
-open class Book(var title: String, var author: String) {
+open class Book(var title: String, var author: String, var year: String) {
     private var currentPage: Int = 0
     open fun readPage() {
         currentPage++
     }
+
+    fun infoTitleAuthor(): Pair<String, String> {
+        return title to author
+    }
+
+    fun infoTitleAuthorYear() : Triple<String, String, String> {
+        return Triple(title, author, year)
+    }
 }
 
-class eBook(title: String, author: String, var format: String = "text") : Book(title, author) {
+class eBook(title: String, author: String, year: String, var format: String = "text") : Book(title, author, year) {
     private var wordCount: Int = 0
     override fun readPage() {
         wordCount += 250
--- src/Book/Main.kt
+++ src/Book/Main.kt
@@ -1,5 +1,10 @@
 package Book
 
 fun main(args: Array<String>) {
-    val book = Book(title = "Kotlin", author = "JetBrains")
+    val book = Book(title = "Kotlin", author = "JetBrains", year =  "2000")
+
+    val titleAuthor = book.infoTitleAuthor()
+    println("Here is your book ${titleAuthor.first} written by ${titleAuthor.second}")
+    val titleAuthorYear = book.infoTitleAuthorYear()
+    println("Here is your book ${titleAuthorYear.first} written by ${titleAuthorYear.second} in ${titleAuthorYear.third}")
 }
