21e549ec18d19f17e06cf95523f1f397bb86b082
Woraphot Chokratanasombat <wc@asiola.com>
2018-05-31 11:09:02 +0700

    [Aquarium] Change: Learn in/out

--- src/Aquarium/Generics/Aquarium.kt
+++ src/Aquarium/Generics/Aquarium.kt
@@ -17,15 +17,30 @@ class LakeWater : WaterSupply(true) {
 class FishStoreSupply : WaterSupply(false)
 
 class Aquarium<T: WaterSupply> (var waterSupply: T) {
-    fun addWater() {
-        check(!waterSupply.needsProcessed) { "Water needs processed" }
+    fun addWater(cleaner: Cleaner<T>? = null) {
+        if (waterSupply.needsProcessed) {
+            cleaner?.clean(waterSupply)
+        }
+
+        println("adding water from ${waterSupply::class.simpleName}")
+    }
+}
+
+interface Cleaner<in T: WaterSupply> {
+    fun clean(waterSupply: T)
+}
+
+class TapWaterCleaner: Cleaner<TapWater> {
+    override fun clean(waterSupply: TapWater) {
+        waterSupply.addChemicalsCleaners()
     }
 }
 
 fun main(args: Array<String>) {
+    val cleaner = TapWaterCleaner()
     val aquariumTap = Aquarium(TapWater())
     aquariumTap.waterSupply.addChemicalsCleaners()
-    aquariumTap.addWater()
+    aquariumTap.addWater(cleaner)
 
 
     val aquariumStore = Aquarium(FishStoreSupply())
